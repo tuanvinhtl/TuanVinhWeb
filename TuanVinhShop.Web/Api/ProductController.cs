b45a934070963a1cdc3d8dc85ef4023c737bfeb1
TuanVinh <tuanvinhtl@gmail.com>
2017-11-15 22:06:00 +0700

    trien khai autofac

---
+++ TuanVinhShop.Web/Api/ProductController.cs
@@ -0,0 +1,91 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TuanVinhShop.Model.Models;
+using TuanVinhShop.Service;
+using TuanVinhShop.Web.Inflastructure.Core;
+
+namespace TuanVinhShop.Web.Api
+{
+    [RoutePrefix("api/product")]
+    public class ProductController : ApiControllerBase
+    {
+        IProductService _productService;
+        public ProductController(IErrorService errorService, IProductService productService) : base(errorService)
+        {
+            this._productService = productService;
+        }
+
+        [Route("getall")]
+        public HttpResponseMessage GetAll(HttpRequestMessage request)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+
+                var result = _productService.GetAll();
+                response = request.CreateResponse(HttpStatusCode.Created, result);
+
+                return response;
+            });
+        }
+
+        [Route("create")]
+        public HttpResponseMessage Create(HttpRequestMessage request, Product product)
+        {
+
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var result = _productService.Add(product);
+                    _productService.SaveChange();
+                    response = request.CreateResponse(HttpStatusCode.Created, result);
+                }
+                return response;
+            });
+
+
+        }
+
+        [Route("update")]
+        public HttpResponseMessage Update(HttpRequestMessage request, Product product)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    _productService.Update(product);
+                    _productService.SaveChange();
+                    response = request.CreateResponse(HttpStatusCode.Created);
+                }
+                return response;
+            });
+        }
+
+        [Route("detele")]
+        public HttpResponseMessage Detele(HttpRequestMessage request, int id)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+
+                var result = _productService.Delete(id);
+                _productService.SaveChange();
+                response = request.CreateResponse(HttpStatusCode.Created, result);
+
+                return response;
+            });
+        }
+    }
+}
---
+++ TuanVinhShop.Web/App_Start/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Autofac;
+using Autofac.Integration.Mvc;
+using System.Reflection;
+using Autofac.Integration.WebApi;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data;
+using TeduShop.Data.Repositories;
+using TuanVinhShop.Service;
+using System.Web.Mvc;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(TuanVinhShop.Web.App_Start.Startup))]
+
+namespace TuanVinhShop.Web.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigAutofac(app);
+        }
+        private void ConfigAutofac(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            // Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
+            builder.RegisterType<DbFactory>().As<IDbFactory>().InstancePerRequest();
+            builder.RegisterType<TuanVinhShopDbContext>().AsSelf().InstancePerRequest();
+
+            // Repositories
+            builder.RegisterAssemblyTypes(typeof(PostCategoryRepository).Assembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces().InstancePerRequest();
+
+            // Services
+            builder.RegisterAssemblyTypes(typeof(PostService).Assembly)
+               .Where(t => t.Name.EndsWith("Service"))
+               .AsImplementedInterfaces().InstancePerRequest();
+
+            Autofac.IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver((IContainer)container);
+        }
+    }
+}
